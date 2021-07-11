868b68d93cda317b90fa7bb6fed409ee1c2493a7
enrico <ensa77@yahoo.com>
2021-07-11 09:11:01 +0200

    first stage

---
+++ mysite/blog/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage,\
+    PageNotAnInteger
+
+from blog.forms import EmployeeForm
+from blog.models import Employee
+
+
+def post_list(request):
+    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published',
+                             publish__year=year, publish__month=month, publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def home(request):
+    return render(request, 'index.html')
+
+# Create your views here.
+
+
+def emp(request):
+    if request.method == "POST":
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('/show')
+            except:
+                pass
+    else:
+        form = EmployeeForm()
+    return render(request, 'index2.html', {'form': form})
+
+
+def show(request):
+    employees = Employee.objects.all()
+    return render(request, "show.html", {'employees': employees})
+
+
+def edit(request, id):
+    employee = Employee.objects.get(id=id)
+    return render(request, 'edit.html', {'employee': employee})
+
+
+def update(request, id):
+    employee = Employee.objects.get(id=id)
+    form = EmployeeForm(request.POST, instance=employee)
+    if form.is_valid():
+        form.save()
+        return redirect("/show")
+    return render(request, 'edit.html', {'employee': employee})
+
+
+def destroy(request, id):
+    employee = Employee.objects.get(id=id)
+    employee.delete()
+    return redirect("/show")
